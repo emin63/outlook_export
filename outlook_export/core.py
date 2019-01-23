d4c6cde0ac90e44e41fa157e07346fbda766bbb4
Emin Martinian <emin.martinian@gmail.com>
2019-01-23 17:41:43 -0500

    More WIP including CSV export mode

--- outlook_export/core.py
+++ outlook_export/core.py
@@ -1,8 +1,13 @@
 """Core functions for exporting data from outlook.
 """
 
-
+import csv
+import os
+import re
 import logging
+
+import json
+
 import win32com.client
 
 
@@ -28,15 +33,87 @@ def get_all(app=None, the_folders=None):
     return result
 
 
-def export_all_msgs(path, app):
+def make_field_map():
+    field_map = {n: n for n in [
+        'SenderName', 'SenderEmailAddress', 'ReceivedTime',
+        'SentOn', 'To', 'Subject', 'Body']}
+    return field_map
+
+def export_msg_to_dict(msg):
+    field_map = make_field_map()
+    data = {}
+    for msg_key, tgt_key in field_map.items():
+        value = getattr(msg, msg_key)
+        xform = getattr(value, 'isoformat', None)
+        if xform is not None:
+            value = xform()
+        assert tgt_key not in data
+        data[tgt_key] = value
+
+    return data
+
+
+def export_msgs_to_json(thing, outdir, max_msgs, names):
+    if not len(thing.Items):
+        return
+    folder_path = thing.FullFolderPath
+    for item in thing.Items:
+        if max_msgs is not None and len(names) >= max_msgs:
+            return
+        data = export_msg_to_dict(item)
+        basename = '%s__%s' % (data['SentOn'], data['Subject'])
+        basename = re.sub('[^-_a-zA-Z_0-9.+@]', '_', basename)
+        full_name = os.path.join(outdir, basename)
+        logging.info('Export message to: %s', full_name)
+        while os.path.exists(full_name):
+            full_name += '_'
+        names.append((folder_path, full_name))
+        with open(full_name, 'w') as out_fd:
+            json.dump(data, out_fd)
+
+
+def export_msgs_to_csv(thing, outdir, max_msgs, names):
+    if not len(thing.Items):
+        return
+    folder_path = thing.FullFolderPath
+    out_file = os.path.join(outdir, 'output.csv')
+    header = list(sorted(make_field_map()))
+    mode = 'a' if os.path.exists(out_file) else 'w'
+    with open(out_file, mode, newline='') as out_fd:
+        writer = csv.writer(out_fd)
+        if mode == 'w':
+            writer.writerow(header)
+        for item in thing.Items:
+            if max_msgs is not None and len(names) >= max_msgs:
+                return
+            data = export_msg_to_dict(item)
+            basename = '%s__%s' % (data['SentOn'], data['Subject'])
+            basename = re.sub('[^-_a-zA-Z_0-9.+@]', '_', basename)
+            full_name = os.path.join(outdir, basename)
+            logging.info('Export message to: %s', full_name)
+            while os.path.exists(full_name):
+                full_name += '_'
+            names.append((folder_path, full_name))
+            writer.writerow([data[n] for n in header])
+
+
+def export_all_msgs(path, app, outdir, max_folders=1, max_msgs=None,
+                    fmt='csv'):
+    if os.path.exists(outdir):
+        raise ValueError('Output directory %s already exists!' % outdir)
+    os.makedirs(outdir)
     all_things = get_all(app=app)
+    names = []
+    folders_exported = 0
+
     for thing in all_things:
         if thing.FullFolderPath == path:
-            FIXME
-            return
-    raise ValueError('Could not find path %s in:\n%s\n' % (
-        path, '\n'.join([item.FullFolderPath for item in all_things])))
+            if fmt == 'json':
+                export_msgs_to_json(thing, outdir, max_msgs, names)
+            elif fmt == 'csv':
+                export_msgs_to_csv(thing, outdir, max_msgs, names)
+            folders_exported += 1
+            if folders_exported >= max_folders:
+                return names
 
-    
-
-    
+    return names
